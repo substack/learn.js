3fc2cdd70e1dd42fa639cdf22f6cdb74e99b43fa
James Halliday <mail@substack.net>
2011-09-27 15:48:36 -0700

    fancier layout with rounded borders

--- browser/entry.js
+++ browser/entry.js
@@ -3,9 +3,9 @@ var $ = require('jquery-browserify');
 $(window).resize(function () {
     var h = $(window).height() - $('#header').height();
     $('#content').height(h).show();
+    $('#header').width($(window).width() - 5).show();
 });
 
 $(window).load(function () {
     $(window).trigger('resize');
 });
-
