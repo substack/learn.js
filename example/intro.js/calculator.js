1b45d80bae319f8c22e3527595f6ca40ac655e8b
James Halliday <mail@substack.net>
2011-10-08 06:45:30 -0700

    note about math functions

--- example/intro.js/calculator.js
+++ example/intro.js/calculator.js
@@ -1,21 +1,29 @@
 var $ = require('jquery-browserify');
 
 module.exports = function (unit, remote) {
-    var finished = false;
-    $('#calc-0').click(function () {
-        var tr = $(this).parents('tr');
-        var val = tr.find('input.value').val();
-        
-        if (val === (3 * 4 + 11 - 3).toString()) {
-            tr.find('.ok').text('ok!');
+    var values = [
+        3 * 4 + 11 - 3,
+        (((111*5)+44*99/555)+33)*0.5,
+        Math.sin(Math.PI / 6)
+    ];
+    
+    values.forEach(function (value, ix) {
+        var finished = false;
+        $('#calc-' + ix).click(function () {
+            var tr = $(this).parents('tr');
+            var val = tr.find('input.value').val();
             
-            if (!finished) {
-                remote.addPoints(5);
-                finished = true;
+            if (val === value.toString()) {
+                tr.find('.ok').text('ok!');
+                
+                if (!finished) {
+                    remote.addPoints(5);
+                    finished = true;
+                }
             }
-        }
-        else {
-            tr.find('.ok').text('not ok');
-        }
+            else {
+                tr.find('.ok').text('not ok');
+            }
+        });
     });
 };
